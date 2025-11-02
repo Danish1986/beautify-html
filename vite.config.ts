e3908e7c9c0a08a45b48008ed0347afe68dd8c5b
Danish1986 <74493061+Danish1986@users.noreply.github.com>
2025-11-03 00:24:37 +0530

    Fix comment formatting in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/beautify-html/', // Important for GitHub Pages
+  base: '/beautify-html/', // Required for GitHub Pages
 });
